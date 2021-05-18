e7fd888a51630a309fddd4a8df283bf9603ff154
thomas <thomas.broussard@local.local>
2021-05-18 12:29:25 +0200

    * updated xml handling in java

--- src/main/java/fr/epita/data/xml/TestReadFromXML.java
+++ src/main/java/fr/epita/data/xml/TestReadFromXML.java
@@ -6,6 +6,10 @@ import java.io.IOException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -14,15 +18,21 @@ import org.xml.sax.SAXException;
 
 public class TestReadFromXML {
 
-	public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
+	public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException, XPathExpressionException {
 		DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
 		Document document = documentBuilder.parse(new File("C:/tmp/data-structuration/menu.xml"));
 		Element documentElement = document.getDocumentElement();
 		NodeList names = documentElement.getElementsByTagName("name");
 
+		XPathFactory xpathFactory = XPathFactory.newInstance();
+		names = (NodeList) xpathFactory.newXPath()
+				.compile("/breakfast_menu/food/name/@lang")
+				.evaluate(document, XPathConstants.NODESET);
 		for (int i = 0; i < names.getLength(); i++) {
 			System.out.println(names.item(i).getTextContent());
 		}
+
+
 
 
 	}
